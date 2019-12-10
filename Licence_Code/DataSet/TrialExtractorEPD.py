23215bccfbc9b1e8fdd6a8f7868ebcf15b2371e3
ioana_onofrei <jeanneomarie01@gmail.com>
2019-12-10 10:52:50 +0200

    average plots

--- Licence_Code/DataSet/TrialExtractorEPD.py
+++ Licence_Code/DataSet/TrialExtractorEPD.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 
 from DataSet.MedataReaderBIN import MetadataReaderBIN
+from DataSet.Segment import Segment, ClassLabel, SegmentLabel
 
 
 class TrialExtractorEPD:
@@ -12,49 +13,61 @@ class TrialExtractorEPD:
         self.codes_timestamps_reader = MetadataReaderBIN(path_epd, file_epd)
         self.extract_trial()
 
+        self.averages = []
+
     def extract_trial(self):
+        self.segments = []
         self.amplitude_array = []
         self.timestamp_array = self.codes_timestamps_reader.event_timestamps
         self.codes_array = self.codes_timestamps_reader.event_codes
 
         self.storage = {128: [], 129: [], 150: []}
-        # for i in range(len(self.codes_timestamps_reader.channel_files)):
-        #     if i != 0 and i != 21 and i != 32:
-        with open(os.path.join(self.path, self.codes_timestamps_reader.channel_files[2].rstrip()), 'rb') as channel_info:
-          values_of_channel  = channel_info.read()
-        self.amplitude = np.frombuffer(values_of_channel, dtype=np.float32)  # schimba cu float
+        for i in range(len(self.codes_timestamps_reader.channel_files)):
+            if i != 0 and i != 21 and i != 32:
+                with open(os.path.join(self.path, self.codes_timestamps_reader.channel_files[i].rstrip()), 'rb') as channel_info:
+                  values_of_channel  = channel_info.read()
+                self.amplitude = np.frombuffer(values_of_channel, dtype=np.float32)
 
-        self.amplitude_array = self.amplitude[self.timestamp_array[0]-1:]
+                self.amplitude_array = self.amplitude[self.timestamp_array[0]-1:]
+                # self.temp_values129 = []
+                contor = 0
+                for i in range(len(self.timestamp_array)-1):
+                    start_timestamp = self.timestamp_array[i]
+                    end_timestamp = self.timestamp_array[i+1]
+                    start_code = self.codes_array[i]
+                    end_code = self.codes_array[i+1]
+                    temp_amplitudes = []
+                    j = start_timestamp
 
-        contor = 0
-        for i in range(len(self.timestamp_array)-1):
-            start_timestamp = self.timestamp_array[i]
-            end_timestamp = self.timestamp_array[i+1]
-            start_code = self.codes_array[i]
-            end_code = self.codes_array[i+1]
-            temp_amplitudes = []
-            j = start_timestamp
+                    while j < end_timestamp:
+                        temp_amplitudes.append(self.amplitude_array[contor])
+                        j = j+1
+                        contor = contor +1
 
-            while j < end_timestamp:
-                temp_amplitudes.append(self.amplitude_array[contor])
-                j = j+1
-                contor = contor +1
+                    if start_code == 128:
+                        oldAmplitudes = self.storage[start_code]
+                        oldAmplitudes.append(temp_amplitudes)
+                        tempDict = {start_code: oldAmplitudes}
+                        self.storage.update(tempDict)
+                        segment = Segment(ClassLabel.LIGHT, SegmentLabel.SPONTANEOUS, i, temp_amplitudes)
+                        self.segments.append(segment)
 
-            if start_code == 128:
-                oldAmplitudes = self.storage[start_code]
-                oldAmplitudes.append(temp_amplitudes)
-                tempDict = {start_code: oldAmplitudes}
-                self.storage.update(tempDict)
+                    if start_code == 129:
+                        oldAmplitudes = self.storage[start_code]
+                        oldAmplitudes.append(temp_amplitudes)
+                        tempDict = {start_code: oldAmplitudes}
+                        self.storage.update(tempDict)
+                        segment = Segment(ClassLabel.LIGHT, SegmentLabel.STIMULUS, i, temp_amplitudes)
+                        self.segments.append(segment)
+                        # np.concatenate(self.temp_values129,temp_amplitudes)
 
-            if start_code == 129:
-                oldAmplitudes = self.storage[start_code]
-                oldAmplitudes.append(temp_amplitudes)
-                tempDict = {start_code: oldAmplitudes}
-                self.storage.update(tempDict)
+                    if start_code == 150:
+                        oldAmplitudes = self.storage[start_code]
+                        oldAmplitudes.append(temp_amplitudes)
+                        tempDict = {start_code: oldAmplitudes}
+                        self.storage.update(tempDict)
+                        segment = Segment(ClassLabel.LIGHT, SegmentLabel.POST_STIMULUS, i, temp_amplitudes)
+                        self.segments.append(segment)
 
-            if start_code == 150:
-                oldAmplitudes = self.storage[start_code]
-                oldAmplitudes.append(temp_amplitudes)
-                tempDict = {start_code: oldAmplitudes}
-                self.storage.update(tempDict)
+
 
--- Licence_Code/Tests/DataSet/Read.py
+++ Licence_Code/Tests/DataSet/Read.py
@@ -2,39 +2,125 @@ import os
 import sys
 import numpy as np
 
-from matplotlib import pyplot as plt
-from DataSet.MetadataReaderEPD import MetadataReaderEPD
+import matplotlib.pyplot as plt
+
+from DataSet.Segment import ClassLabel, SegmentLabel, Segment
 from DataSet.TrialExtractorEPD import TrialExtractorEPD
 
-# project_path = os.path.join('..', '..')
-# data_dir = os.path.join(project_path, 'Data', 'M017_0002_sorted_full')
-# sys.path.append(project_path)
-# ssd_file = 'M017_S001_SRCS3L_25,50,100_0002.ssd'
-# mr = MetadataReader(data_dir, ssd_file)
-# print(mr.no_units)
-# mr.print_metadata()
-
 project_path = os.path.join('..', '..')
-# data_dir = os.path.join(project_path, 'Data', 'M017_0002_sorted_full')
 data_dir = os.path.join(project_path, 'Data', '')
 sys.path.append(project_path)
 ssd_file_deep = 'M014_S001_SRCS3L_25,50,100_0002.epd'
 ssd_file_medium = 'M014_S001_SRCS3L_25,50,100_0003.epd'
 ssd_file_light = 'M014_S001_SRCS3L_25,50,100_0004.epd'
-# mr = MetadataReaderEPD(data_dir, ssd_file)
-# mr.print_metadata()
 
+# constant for establishing the limits of the plots
+scale_conts = 10
+
+
+# PRINT LIGHT, MEDIUM AND DEEP PLOTS FOR A CHANNEL
+
+# # DEEP
 trial_deep = TrialExtractorEPD(data_dir, ssd_file_deep)
-print(len(trial_deep.storage[129]))
+# print(len(trial_deep.storage[129]))
 # # cleaning the storage to have only arrays with length 2672, so delete the last element if the size is 2673
 for i in range(len(trial_deep.storage[129])):
     # print(len(trial.storage[129][i]))
     if len(trial_deep.storage[129][i]) > 2672:
         trial_deep.storage[129][i] = trial_deep.storage[129][i][:-1]
 
-# myArray = np.array(trial.storage[129])
-# myMatrix = np.asmatrix(myArray)
+myArray_deep = np.array(trial_deep.storage[129])
+myMatrix_deep = np.asmatrix(myArray_deep)
+avgMatrix_deep = myMatrix_deep.mean(0)
+avgArray_deep = np.squeeze(np.asarray(avgMatrix_deep))
+# avgArray_deep_scaled = np.interp(avgArray_deep,-6,6, )
+
+plt.plot(avgArray_deep)
+plt.ylim(-scale_conts, scale_conts)
+plt.title("channel 2 deep", fontdict=None)
+plt.xlabel("samples")
+plt.ylabel("voltages")
+plt.show()
 #
+# # MEDIUM
+# trial_medium = TrialExtractorEPD(data_dir, ssd_file_medium)
+# print(len(trial_medium.storage[129]))
+# # # cleaning the storage to have only arrays with length 2672, so delete the last element if the size is 2673
+# for i in range(len(trial_medium.storage[129])):
+#     # print(len(trial_medium.storage[129][i]))
+#     if len(trial_medium.storage[129][i]) != 2672:
+#         trial_medium.storage[129][i] = trial_medium.storage[129][i][:-1]
+#
+# myArray_medium = np.array(trial_medium.storage[129])
+# myMatrix_medium = np.asmatrix(myArray_medium)
+# avgMatrix_medium = myMatrix_medium.mean(0)
+# avgArray_medium = np.squeeze(np.asarray(avgMatrix_medium))
+#
+# plt.plot(avgArray_medium)
+# plt.ylim(-scale_conts, scale_conts)
+# plt.title("channel 2 medium", fontdict=None)
+# plt.xlabel("samples")
+# plt.ylabel("voltages")
+# plt.show()
+#
+# # LIGHT
+trial_light = TrialExtractorEPD(data_dir, ssd_file_deep)
+# print(len(trial_light.storage[129]))
+# # # # cleaning the storage to have only arrays with length 2672, so delete the last element if the size is 2673
+# for i in range(len(trial_light.storage[129])):
+#     print(len(trial_light.storage[129][i]))
+#     if len(trial_light.storage[129][i]) != 2672:
+#         trial_light.storage[129][i] = trial_light.storage[129][i][:-1]
+#
+# myArray_light = np.array(trial_light.storage[129])
+# myMatrix_light = np.asmatrix(myArray_light)
+# avgMatrix_light = myMatrix_light.mean(0)
+# avgArray_light = np.squeeze(np.asarray(avgMatrix_light))
+#
+# plt.plot(avgArray_light)
+# plt.ylim(-scale_conts, scale_conts)
+# plt.title("channel 2 light", fontdict=None)
+# plt.xlabel("samples")
+# plt.ylabel("voltages")
+# plt.show()
+
+# SAVE THE SEGMENT
+
+# LIGHT
+# trial_light = TrialExtractorEPD(data_dir, ssd_file_light)
+# print(len(trial_light.segments))
+
+for i in range(len(trial_light.storage[129])):
+    if len(trial_light.storage[129][i]) != 2672:
+        trial_light.storage[129][i] = trial_light.storage[129][i][:-1]
+
+myArray_light_STIMULUS = np.array(trial_light.storage[129])
+
+my_averages =[]
+contor = 0
+for i in range(30):
+    my_array = []
+    for j in range(240):
+        my_array.append(myArray_light_STIMULUS[contor])
+        contor = contor + 1
+
+    for m in range(len(my_array)):
+        if len(my_array[i]) != 2672:
+            my_array[i] = my_array[i][:-1]
+    myMatrix_light = np.asmatrix(my_array)
+    avgMatrix_light = myMatrix_light.mean(0)
+    avgArray_light = np.squeeze(np.asarray(avgMatrix_light))
+    my_averages.append(avgArray_light)
+
+print(len(my_averages[0]))
+# media canalelor
+myArray_light = np.array(trial_light.storage[129])
+myMatrix_average = np.asmatrix(my_averages)
+avgMatrix_light_avg = myMatrix_average.mean(0)
+avgArray_light_final = np.squeeze(np.asarray(avgMatrix_light_avg))
+print(len(avgArray_light_final))
+plt.plot(avgArray_light_final)
+plt.show()
 # matriceFinala = np.zeros(shape=(240,30), dtype=myArray.dtype)
 # for i in range(240):
 #     myVec = []
@@ -51,75 +137,3 @@ for i in range(len(trial_deep.storage[129])):
 #         # np.concatenate([tempVec,matriceFinala[i][j]])
 #         np.append(tempVec, matriceFinala[i][j])
 #     matriceTemp[i] = tempVec
-
-myArray_deep = np.array(trial_deep.storage[129])
-myMatrix_deep = np.asmatrix(myArray_deep)
-avgMatrix_deep = myMatrix_deep.mean(0)
-avgArray_deep = np.squeeze(np.asarray(avgMatrix_deep))
-
-plt.plot(avgArray_deep)
-plt.title("channel 2 deep", fontdict=None,)
-plt.show()
-
-
-trial_medium = TrialExtractorEPD(data_dir, ssd_file_medium)
-print(len(trial_medium.storage[129]))
-# # cleaning the storage to have only arrays with length 2672, so delete the last element if the size is 2673
-for i in range(len(trial_medium.storage[129])):
-    # print(len(trial_medium.storage[129][i]))
-    if len(trial_medium.storage[129][i]) != 2672:
-        trial_medium.storage[129][i] = trial_medium.storage[129][i][:-1]
-
-myArray_medium = np.array(trial_medium.storage[129])
-myMatrix_medium = np.asmatrix(myArray_medium)
-avgMatrix_medium = myMatrix_medium.mean(0)
-avgArray_medium = np.squeeze(np.asarray(avgMatrix_medium))
-
-plt.plot(avgArray_medium)
-plt.title("channel 2 medium", fontdict=None)
-plt.show()
-
-
-trial_light = TrialExtractorEPD(data_dir, ssd_file_light)
-print(len(trial_light.storage[129]))
-# # # cleaning the storage to have only arrays with length 2672, so delete the last element if the size is 2673
-for i in range(len(trial_light.storage[129])):
-    print(len(trial_light.storage[129][i]))
-    if len(trial_light.storage[129][i]) != 2672:
-        trial_light.storage[129][i] = trial_light.storage[129][i][:-1]
-
-myArray_light = np.array(trial_light.storage[129])
-myMatrix_light = np.asmatrix(myArray_light)
-avgMatrix_light = myMatrix_light.mean(0)
-avgArray_light = np.squeeze(np.asarray(avgMatrix_light))
-
-plt.plot(avgArray_light)
-plt.title("channel 2 light", fontdict=None)
-plt.show()
-
-# print("SIZEEE")
-#
-# print(len(trial.storage[129]))
-#
-# for i in range(len(trial.storage[129])):
-#     print(len(trial.storage[129][i]))
-#
-# average_segment = []
-# contor = 0
-# sum = 0
-# for i in range(len(trial.storage[129])):
-#     sum += trial.storage[129][i][contor]
-#     contor += 1
-
-# for i in range(len(average_segment)):
-#     print(average_segment[i])
-# print(trial.storage[129][i])
-
-# dict = {}
-# dict[0] = [[1,2,3]]
-# dict[1] = []
-# # tempList = dict[0]
-# # tempList.append([5,6])
-# # tempD = {0 : tempList}
-# # dict.update(tempD)
-# print(dict[0])
