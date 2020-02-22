d8c71939ab9f57c5a118ba49e12ee232ca4e4f61
ioana_onofrei <jeanneomarie01@gmail.com>
2020-02-22 22:48:07 +0200

    lbg updated

--- Licence_Code/TESPAR/CALL_LBG.py
+++ Licence_Code/TESPAR/CALL_LBG.py
@@ -4,9 +4,9 @@
 '''
 import numpy as np
 
-from VQ_REMAKE.LBG import VQ_LGB
-
 # citeste o matrice din orice fisier
+from TESPAR.LBG import VQ_LGB
+
 input_matrix = np.loadtxt(fname='../Statistics/distributions_100_50/global_matrix_DS.txt', dtype='i')
 
 lbg = VQ_LGB(k=32, alpha=0.00005, t=10000, freq_replacer=0.00001, scale_s=1, epsilon= 0.1)
