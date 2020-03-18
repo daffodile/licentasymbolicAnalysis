f257c914a7ee5a57facec4e2ab52526a8594f4d3
narcici <narcisa.teodora16@gmail.com>
2020-03-18 20:25:17 +0200

    separated train test in general from TESPAR

--- Licence_Code/classification/svm/Train_and_Test_TESPAR.py
+++ Licence_Code/classification/svm/Train_and_Test_TESPAR.py
@@ -4,24 +4,6 @@ from numpy.random.mtrand import permutation
 from sklearn.model_selection import train_test_split
 
 
-# try to use train and test splitting on this example
-# Y = [1, 0, 1, 1, 0, 1, 1, 0, 0, 0]  # 1 is for odds and 0 for evens
-# X = [[1, 3, 5, 7], [2, 4, 6, 8], [11, 13, 15, 17], [21, 23, 25, 27], [12, 14, 16, 18], [31, 33, 35, 37],
-#      [41, 43, 45, 47], [22, 24, 26, 28], [32, 34, 36, 38], [42, 44, 46, 48]]
-#
-# df = pd.DataFrame(X)
-# y = np.array(Y)
-#
-# # create training and testing vars
-# X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, shuffle=False)
-# print(X_train)
-# print(y_train.shape)
-# print(X_test.shape)
-# print(y_test.shape)
-
-# next we use X_train and y_train
-
-
 class TrainTestSplitting:
     def __init__(self, inputData, encoding):
         self.inputData = inputData
@@ -37,18 +19,8 @@ class TrainTestSplitting:
             X = []
             Y = []
             for j in range(len(self.inputData.result.arrays[i].array)):
-                # call encoding as param
-
-                X.append(self.inputData.result.arrays[i].array[j])
+                X.append(np.asarray(self.encoding.get_a(self.inputData.result.arrays[i].array[j], 1)).ravel())
                 Y.append(self.inputData.result.arrays[i].name)
-
-            # REMAINS TO BE SEEN
-            # shuffle trials
-            # X = np.asarray(X)
-            # Y = np.asarray(Y)
-            # perm = permutation(len(X))
-            # X = X[perm]
-            # Y = Y[perm]
 
             df = pd.DataFrame(X)
             y = np.array(Y)
@@ -71,9 +43,5 @@ class TrainTestSplitting:
         perm = permutation(len(X_test))
         X_test = X_test[perm]
         Y_test = Y_test[perm]
-
-        # save train deep light
-        # save test deep light
-        # save this to folders files
 
         return X_train, X_test, Y_train, Y_test
