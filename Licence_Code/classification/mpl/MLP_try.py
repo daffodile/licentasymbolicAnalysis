f87a326587b71433a54142f67d6d39cd6dad8f10
narcici <narcisa.teodora16@gmail.com>
2020-04-29 18:55:04 +0300

    first try of mlp
    narci_svm

--- Licence_Code/classification/mpl/MLP_try.py
+++ Licence_Code/classification/mpl/MLP_try.py
@@ -1,32 +1,49 @@
 import os
-import random
 
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
 
 from feature_extraction.TESPAR.Encoding import Encoding
 from input_reader.InitDataSet import InitDataSet
-from utils.DataSpliting import obtain_features_labels_from_doa
+from utils.DataSpliting import obtain_features_labels_from_doa, train_test_doa_remake_balanced
 
-# encoding = Encoding('./../../data_to_be_saved/alphabet_3.txt')
-#
-# data_dir = os.path.join('..', '..')
-#
-# initialization = InitDataSet(data_dir=data_dir, levels=['deep', 'light'])
-# doas = initialization.get_dataset_as_doas()
-#
-# X_train, y_train = obtain_features_labels_from_doa(doas, 2, 'spontaneous', encoding)
-#
-# model = MLPClassifier(solver='lbfgs', alpha=1e-5, random_state=1)
-#
-# skf = StratifiedKFold()
-# cv_result = cross_val_score(model, X_train, y_train, cv=skf, scoring="accuracy")
-# print(cv_result)
-# average_res = cv_result.mean()
-# print(average_res)
+encoding = Encoding('./../../data_to_be_saved/alphabet_3.txt')
 
-trials_common_to_all = [i for i in range(240)]
-print(trials_common_to_all)
+data_dir = os.path.join('..', '..')
 
-random.Random(4).shuffle(trials_common_to_all)
-print(trials_common_to_all)
+initialization = InitDataSet(data_dir=data_dir, levels=['deep', 'light'])
+doas = initialization.get_dataset_as_doas()
+
+doas_train, doas_test = train_test_doa_remake_balanced(doas)
+
+X_train, y_train = obtain_features_labels_from_doa(doas_train, 2, 'spontaneous', encoding)
+X_test, y_test = obtain_features_labels_from_doa(doas_test, 2, 'spontaneous', encoding)
+
+parameter_space = {
+    'hidden_layer_sizes': [(50, 50, 50), (50, 100, 50), (100, 100, 100), (10, 10, 10), (100,)],
+    'activation': ['tanh', 'relu'],
+    'solver': ['lbfgs', 'sgd', 'adam'],
+    'alpha': [0.0001, 0.05, 0.1, 0.5],
+    'learning_rate': ['constant', 'adaptive', 'invscaling'],
+}
+model = MLPClassifier(max_iter=100)
+
+clf = GridSearchCV(model, parameter_space, n_jobs=-1, cv=5)
+clf.fit(X_train, y_train)
+
+# Best paramete set
+print('Best parameters found:\n', clf.best_params_)
+
+# All results
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+print('Results on the test set:')
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
---
+++ Licence_Code/classification/mpl/Run_MLP.py
@@ -0,0 +1,113 @@
+import os
+
+import numpy as np
+from pandas import DataFrame
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+
+from feature_extraction.TESPAR.Encoding import Encoding
+from feature_extraction.TESPAR.EncodingCheckBursts import EncodingCheckBursts
+from input_reader.InitDataSet import InitDataSet
+from input_reader.InitDataSetWithBurstsFlags import InitDataSetWithBurstsFlags
+from utils.DataSpliting import train_test_doa_remake_balanced, obtain_S_features_from_doa_with_bursts_flags, \
+    obtain_features_labels_from_doa
+from utils.MarkOutsidersWithBurstsFlags import remove_bursted_trials_when_segment
+from utils.MarkOutsiderWithBurstFlags_SeparateThresholds import mark_bursts_regions
+
+csv_file = "mlp_run_20.csv"
+csv_results = "mlp_run_20_avr.csv"
+
+output_name = "mlp_results_20.txt"
+output_file = open(output_name, 'w')
+
+output_file.write("Run MLP 26 apr \n")
+output_file.write("2 classes LIGHT DEEP, 20 repetitions, independent per seg: spon or stim, each channel\n")
+output_file.write(
+    "{'activation': 'tanh', 'alpha': 0.05, 'hidden_layer_sizes': (100,), 'learning_rate': 'constant', 'solver': 'sgd'} \n")
+
+# how many models to train a for a channel-segment pair
+run_nr = 20
+
+all_channels = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32]
+
+# all_channels = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+segments = ['spontaneous', 'stimulus']
+
+# data frame that keeps all runs for all channels, that will be added to .csv file
+column_names = ['channel', 'segment', 'accuracy', 'f1-score']
+df_all = DataFrame(columns=column_names)
+df_all.to_csv(csv_file, mode='a', header=True)
+
+# DEFAULT DATASET CONTAINS DEEP LIGHT CLASSES
+data_dir = os.path.join('..', '..')
+initialization = InitDataSet(data_dir=data_dir)
+doas = initialization.get_dataset_as_doas()
+
+encoding = Encoding('./../../data_to_be_saved/alphabet_3.txt')
+
+accuracies = [[[] for i in range(len(all_channels))] for j in range(len(segments))]
+f1scores = [[[] for i in range(len(all_channels))] for j in range(len(segments))]
+
+for run in range(run_nr):
+    print('************************RUN ' + str(run) + '************************')
+    # firstly split the input into train test
+    doas_train, doas_test = train_test_doa_remake_balanced(doas)
+
+    for ind_segment, segment in enumerate(segments):
+        for chn_ind, channel in enumerate(all_channels):
+            print("start running for channel " + str(channel) + ' ' + segment + '\n')
+            output_file.write(f'run {run} channel {channel} \n')
+
+            # obtain_A_features_from_doa_with_bursts_frags(doas, channel_number, segment, encoding, selected_symbols=None):
+            X_train, y_train = obtain_features_labels_from_doa(doas_train, channel, segment, encoding)
+            x_test, y_test = obtain_features_labels_from_doa(doas_test, channel, segment, encoding)
+
+            model = MLPClassifier(activation='tanh', alpha=0.05, solver='sgd')
+
+            model.fit(X_train, y_train)
+            predictions = model.predict(x_test)
+
+            report = classification_report(y_test, predictions, output_dict=True)
+
+            print(classification_report(y_test, predictions))
+            print(confusion_matrix(y_test, predictions))
+
+            output_file.write(classification_report(y_test, predictions))
+            np.savetxt(output_file, np.array(confusion_matrix(y_test, predictions)), fmt="%s", newline=' ')
+            output_file.write('\n')
+
+            acc = report['accuracy']
+            f1sc = report['weighted avg']['f1-score']
+            accuracies[ind_segment][chn_ind].append(acc)
+            f1scores[ind_segment][chn_ind].append(f1sc)
+
+            # calculate and write the mean  and std_dev of the average & f1-score
+            df_all = df_all.append(
+                {'channel': channel, 'segment': segment, 'accuracy': acc, 'f1-score': f1sc}, ignore_index=True)
+
+            # print('debug')
+    df_all.to_csv(csv_file, mode='a', header=False)
+    df_all = df_all.iloc[0:0]
+
+output_file.close()
+
+# data frame that keeps avr and std of the runs
+columns = ['channel', 'segment', 'acc avr', 'acc std_dev', 'f1-sc avr', 'f1-sc std_dev']
+df_results = DataFrame(columns=columns)
+df_results.to_csv(csv_results, mode='a', header=True)
+
+for ind_segment, segment in enumerate(segments):
+    for ind_ch, channel in enumerate(all_channels):
+        acc_avr = np.mean(np.array(accuracies[ind_segment][ind_ch]))
+        acc_std = np.std(np.array(accuracies[ind_segment][ind_ch]))
+
+        f1_avr = np.mean(np.array(f1scores[ind_segment][ind_ch]))
+        f1_std = np.std(np.array(f1scores[ind_segment][ind_ch]))
+        df_results = df_results.append({'channel': channel, 'segment': segment, 'acc avr': acc_avr,
+                                        'acc std_dev': acc_std, 'f1-sc avr': f1_avr, 'f1-sc std_dev': f1_std},
+                                       ignore_index=True)
+
+df_results.to_csv(csv_results, mode='a', header=False)
