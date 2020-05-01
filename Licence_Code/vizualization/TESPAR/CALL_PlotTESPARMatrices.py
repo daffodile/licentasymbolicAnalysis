d55ecc290d2f16d46311e9deea5cb7965f05501a
narcici <narcisa.teodora16@gmail.com>
2020-05-01 19:58:14 +0300

    calculate mean std_Dev plot tespar matrices
    narci_svm

--- Licence_Code/vizualization/TESPAR/CALL_PlotTESPARMatrices.py
+++ Licence_Code/vizualization/TESPAR/CALL_PlotTESPARMatrices.py
@@ -1,19 +1,40 @@
 import os
 
 from feature_extraction.TESPAR.Encoding import Encoding
+from feature_extraction.TESPAR.EncodingCheckBursts import EncodingCheckBursts
 from input_reader.InitDataSet import InitDataSet
+from input_reader.InitDataSetWithBurstsFlags import InitDataSetWithBurstsFlags
+from utils.MarkOutsiderWithBurstFlags_SeparateThresholds import mark_bursts_regions
+from utils.MarkOutsidersWithBurstsFlags import remove_bursted_trials_when_segment
 from vizualization.TESPAR.PlotTESPARMatrices import get_channel_matrix_A
 
 current_dir = os.path.join('..', '..')
 
-levels = ['deep2', 'medium3', 'light4']
+# levels = ['deep2', 'medium3', 'light4']
+levels = ['deep2', 'light4']
+# levels = ['deep1', 'medium3', 'medium5']
+# levels = ['deep1']
 
 # def __init__(self, current_directory, subject_directory, filtering_directory, levels=['deep', 'medium', 'light'], trials_to_skip=None):
-initialization = InitDataSet(current_directory=current_dir, subject_directory="m014", filtering_directory="classic",
+initialization = InitDataSetWithBurstsFlags(current_directory=current_dir, subject_directory="m014", filtering_directory="highpass10",
                              levels=levels)
+
 doas = initialization.get_dataset_as_doas()
 
-encoding = Encoding('./../../data_to_be_saved/alphabet_3.txt')
+# current_dir = os.path.join('..', '..')
+#
+# levels = ['deep1', 'deep2', 'medium3', 'light4', 'medium5']
+#
+# initialization = InitDataSet(current_directory=current_dir, subject_directory="m014", filtering_directory="highpass10",
+#                              levels=levels)
+# doas = initialization.get_dataset_as_doas()
+# print('doas is initialized')
+
+mark_bursts_regions(doas)
+
+remove_bursted_trials_when_segment(doas)
+
+encoding = EncodingCheckBursts('./../../data_to_be_saved/alphabet_3.txt')
 
 channels = [2, 6, 20, 29]
 
