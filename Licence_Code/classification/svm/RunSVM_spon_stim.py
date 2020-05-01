99b72b2028cdeed76049fe635215fed93e896bc3
narcici <narcisa.teodora16@gmail.com>
2020-05-01 19:57:35 +0300

    run deep-medium classif to have a comparison
    narci_svm

--- Licence_Code/classification/svm/RunSVM_spon_stim.py
+++ Licence_Code/classification/svm/RunSVM_spon_stim.py
@@ -1,23 +1,23 @@
 import os
-
 import numpy as np
 from pandas import DataFrame
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
-
-####### to change for each  classifier this 3 files #################################
 from feature_extraction.TESPAR.Encoding import Encoding
 from input_reader.InitDataSet import InitDataSet
-from utils.DataSpliting import train_test_doa_check_trials, obtain_A_features_from_doa
+from utils.DataSpliting import train_test_doa_check_trials, obtain_A_features_from_doa, train_test_doa_remake_balanced
 
-csv_file = "svm_39_2segs.csv"
-csv_results = "svm_30_2segs_avr.csv"
-# open file to write the indices of  each splitting
-indexes_file = "svm_30_2segs_indexes.txt"
-write_file = open(indexes_file, "w")
+csv_file = "svm_20_deep_medium_2segs.csv"
+csv_results = "svm_20_deep_medium_2segs_avr.csv"
 
-# how many models to train a for a channel-segment pair
-run_nr = 30
+output_name = "results_svm_20_deep_medium_2segs.txt"
+output_file = open(output_name, 'w')
+
+output_file.write("Classify bt 2 levels, before filtering and without marking bursts 30 apr \n")
+output_file.write("DEEP MEDIUM A matrix \n")
+output_file.write("train_test_doa_remake_balanced 80% for train \n")
+
+run_nr = 20
 
 all_channels = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30,
                 31, 32]
@@ -29,12 +29,15 @@ column_names = ['channel', 'segment', 'accuracy', 'f1-score']
 df_all = DataFrame(columns=column_names)
 df_all.to_csv(csv_file, mode='a', header=True)
 
-
 encoding = Encoding('./../../data_to_be_saved/alphabet_3.txt')
 
 data_dir = os.path.join('..', '..')
 
-initialization = InitDataSet(data_dir=data_dir)
+levels = ['deep2', 'medium3']
+
+# def __init__(self, current_directory, subject_directory, filtering_directory, levels=['deep', 'medium', 'light'], trials_to_skip=None):
+initialization = InitDataSet(current_directory=data_dir, subject_directory="m014", filtering_directory="classic",
+                             levels=levels)
 doas = initialization.get_dataset_as_doas()
 
 accuracies = [[] for i in range(len(all_channels))]
@@ -43,13 +46,11 @@ f1scores = [[] for i in range(len(all_channels))]
 for run in range(run_nr):
     print('************************RUN ' + str(run) + '************************')
     # firstly split the input into train test
-    doas_train, doas_test, ind_test = train_test_doa_check_trials(doas, 0.2)
-
-    np.savetxt(write_file, np.array(ind_test), fmt="%s", newline=' ')
-    write_file.write('\n')
+    doas_train, doas_test = train_test_doa_remake_balanced(doas)
 
     for chn_ind, channel in enumerate(all_channels):
         print("start running for channel " + str(channel))
+        output_file.write(f'####### run {run} channel {channel} #########\n')
 
         # obtain_A_features_from_doa(doas, channel_number, encoding, segments=['spontaneous', 'stimulus'],
         #                                selected_symbols=None):
@@ -62,6 +63,13 @@ for run in range(run_nr):
         predictions = model.predict(x_test)
 
         report = classification_report(y_test, predictions, output_dict=True)
+
+        print(classification_report(y_test, predictions))
+        print(confusion_matrix(y_test, predictions))
+
+        output_file.write(classification_report(y_test, predictions))
+        np.savetxt(output_file, np.array(confusion_matrix(y_test, predictions)), fmt="%s", newline=' ')
+        output_file.write('\n')
 
         acc = report['accuracy']
         f1sc = report['weighted avg']['f1-score']
@@ -76,7 +84,7 @@ for run in range(run_nr):
     df_all.to_csv(csv_file, mode='a', header=False)
     df_all = df_all.iloc[0:0]
 
-write_file.close()
+output_file.close()
 
 # data frame that keeps avr and std of the runs
 columns = ['channel', 'segment', 'acc avr', 'acc std_dev', 'f1-sc avr', 'f1-sc std_dev']
